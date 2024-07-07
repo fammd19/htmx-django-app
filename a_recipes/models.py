391f0bff6710abf9fc96a37d0057149cbe7a5778
Fiona Mellink <fimellink@gmail.com>
2024-07-07 22:06:22 +1000

    Adds second model and styling

--- a_recipes/models.py
+++ a_recipes/models.py
@@ -8,15 +8,15 @@ class Recipe(models.Model):
     cook_time=models.CharField(max_length=50)
 
     def __str__(self):
-        return f"ID {self.id}: {self.title}"
+        return f"{self.title}"
 
     class Meta:
         ordering = ['title']
 
-# class Ingredients(models.Model):
-#     unit_of_measure=models.CharField(max_length=50)
-#     quantity=models.DecimalField(decimal_places=2)
-#     ingredient=models.IntegerField(max_length=5)
-#     notes=models.IntegerField(max_length=5)
-#     recipe=models.OneToOneField(Recipe, on_delete=models.CASCADE,primary_key=True)
+class Ingredient(models.Model):
+    unit_of_measure=models.CharField(max_length=50)
+    quantity=models.DecimalField(decimal_places=2,max_digits=6)
+    ingredient=models.CharField(max_length=5)
+    notes=models.CharField(max_length=50,default=None, blank=True, null=True)
+    recipe=models.ForeignKey(Recipe, null=False, on_delete=models.CASCADE)
 
